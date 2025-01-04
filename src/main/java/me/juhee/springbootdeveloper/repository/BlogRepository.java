e9bcce64d151e1b1d8e6363f252a7c6cc813fee8
ljhee92 <weilainib7@gmail.com>
2025-01-04 21:54:21 +0900

    feat: 리프레시 토큰, 토큰 필터 구현

--- src/main/java/me/juhee/springbootdeveloper/repository/BlogRepository.java
+++ src/main/java/me/juhee/springbootdeveloper/repository/BlogRepository.java
@@ -2,8 +2,6 @@ package me.juhee.springbootdeveloper.repository;
 
 import me.juhee.springbootdeveloper.domain.Article;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
-@Repository
 public interface BlogRepository extends JpaRepository<Article, Long> {
 }
