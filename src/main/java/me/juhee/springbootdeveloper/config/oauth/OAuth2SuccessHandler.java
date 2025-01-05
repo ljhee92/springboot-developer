e1fec90cabc8af04142d98b4124e6f40356a7f5b
ljhee92 <weilainib7@gmail.com>
2025-01-05 14:32:12 +0900

    feat: OAuth2 설정

---
+++ src/main/java/me/juhee/springbootdeveloper/config/oauth/OAuth2SuccessHandler.java
@@ -0,0 +1,79 @@
+package me.juhee.springbootdeveloper.config.oauth;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import me.juhee.springbootdeveloper.config.jwt.TokenProvider;
+import me.juhee.springbootdeveloper.domain.RefreshToken;
+import me.juhee.springbootdeveloper.domain.User;
+import me.juhee.springbootdeveloper.repository.RefreshTokenRepository;
+import me.juhee.springbootdeveloper.service.UserService;
+import me.juhee.springbootdeveloper.util.CookieUtil;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.oauth2.core.user.OAuth2User;
+import org.springframework.security.web.authentication.SimpleUrlAuthenticationSuccessHandler;
+import org.springframework.stereotype.Component;
+import org.springframework.web.util.UriComponentsBuilder;
+
+import java.io.IOException;
+import java.time.Duration;
+
+@RequiredArgsConstructor
+@Component
+public class OAuth2SuccessHandler extends SimpleUrlAuthenticationSuccessHandler {
+    public static final String REFRESH_TOKEN_COOKIE_NAME = "refresh_token";
+    public static final Duration REFRESH_TOKEN_DURATION = Duration.ofDays(14);
+    public static final Duration ACCESS_TOKEN_DURATION = Duration.ofDays(1);
+    public static final String REDIRECT_PATH = "/articles";
+
+    private final TokenProvider tokenProvider;
+    private final RefreshTokenRepository refreshTokenRepository;
+    private final OAuth2AuthorizationRequestBasedOnCookieRepository authorizationRequestRepository;
+    private final UserService userService;
+
+    @Override
+    public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response,
+                                        Authentication authentication) throws IOException {
+        OAuth2User oAuth2User = (OAuth2User) authentication.getPrincipal();
+        User user = userService.findByEmail(oAuth2User.getAttributes().get("email").toString());
+
+        // 리프레시 토큰 생성 -> 저장 -> 쿠키에 저장
+        String refreshToken = tokenProvider.generateToken(user, REFRESH_TOKEN_DURATION);
+        saveRefreshToken(user.getId(), refreshToken);
+        addRefreshTokenToCookie(request, response, refreshToken);
+        // 액세스 토큰 생성 -> 패스에 액세스 토큰 추가
+        String accessToken = tokenProvider.generateToken(user, ACCESS_TOKEN_DURATION);
+        String targetUrl = getTargetUrl(accessToken);
+        // 인증 관련 설정 값, 쿠키 제거
+        clearAuthenticationAttributes(request, response);
+        // 리다이렉트
+        getRedirectStrategy().sendRedirect(request, response, targetUrl);
+    }
+
+    private void saveRefreshToken(Long userId, String newRefreshToken) {
+        RefreshToken refreshToken = refreshTokenRepository.findByUserId(userId)
+                .map(entity -> entity.update(newRefreshToken))
+                .orElse(new RefreshToken(userId, newRefreshToken));
+
+        refreshTokenRepository.save(refreshToken);
+    }
+
+    private void addRefreshTokenToCookie(HttpServletRequest request, HttpServletResponse response,
+                                         String refreshToken) {
+        int cookieMaxAge = (int) REFRESH_TOKEN_DURATION.getSeconds();
+        CookieUtil.deleteCookie(request, response, REFRESH_TOKEN_COOKIE_NAME);
+        CookieUtil.addCookie(response, REFRESH_TOKEN_COOKIE_NAME, refreshToken, cookieMaxAge);
+    }
+
+    private String getTargetUrl(String accessToken) {
+        return UriComponentsBuilder.fromUriString(REDIRECT_PATH)
+                .queryParam("token", accessToken)
+                .build()
+                .toUriString();
+    }
+
+    private void clearAuthenticationAttributes(HttpServletRequest request, HttpServletResponse response) {
+        super.clearAuthenticationAttributes(request);
+        authorizationRequestRepository.removeAuthorizationRequestCookies(request, response);
+    }
+}
---
+++ src/main/java/me/juhee/springbootdeveloper/config/oauth/OAuth2UserCustomService.java
@@ -0,0 +1,38 @@
+package me.juhee.springbootdeveloper.config.oauth;
+
+import lombok.RequiredArgsConstructor;
+import me.juhee.springbootdeveloper.domain.User;
+import me.juhee.springbootdeveloper.repository.UserRepository;
+import org.springframework.security.oauth2.client.userinfo.DefaultOAuth2UserService;
+import org.springframework.security.oauth2.client.userinfo.OAuth2UserRequest;
+import org.springframework.security.oauth2.core.OAuth2AuthenticationException;
+import org.springframework.security.oauth2.core.user.OAuth2User;
+import org.springframework.stereotype.Service;
+
+import java.util.Map;
+
+@RequiredArgsConstructor
+@Service
+public class OAuth2UserCustomService extends DefaultOAuth2UserService {
+    private final UserRepository userRepository;
+
+    @Override
+    public OAuth2User loadUser(OAuth2UserRequest userRequest) throws OAuth2AuthenticationException {
+        OAuth2User user = super.loadUser(userRequest);
+        saveOrUpdate(user);
+        return user;
+    }
+
+    private User saveOrUpdate(OAuth2User oAuth2User) {
+        Map<String, Object> attributes = oAuth2User.getAttributes();
+        String email = String.valueOf(attributes.get("email"));
+        String name = String.valueOf(attributes.get("name"));
+        User user = userRepository.findByEmail(email)
+                .map(entity -> entity.update(name))
+                .orElse(User.builder()
+                        .email(email)
+                        .nickname(name)
+                        .build());
+        return userRepository.save(user);
+    }
+}
